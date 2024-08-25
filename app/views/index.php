68575fc9583c74908e8b194a00c728d67021b8a3
manguito088 <andreaequispe@gmail.com>
2024-08-24 22:30:23 -0500

    mi_primer_commit_andrea

--- app/views/index.php
+++ app/views/index.php
@@ -3,6 +3,6 @@
 <html lang="en" class="" style="height: auto;">
 <?php require_once(base_app . '../app/includes/header.php') ?>
 <body>
-    <h1>HOLA AMIGO, INICIASTE SESIÓN CORRECTAMENTE</h1>
+    <h1>HOLA AMIGE, INGRESASTE CORRECTAMENTE</h1>
 </body>
 </html>
