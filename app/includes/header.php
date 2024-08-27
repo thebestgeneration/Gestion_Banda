96dcd5cde08eacd26727ff5256e85f301a2d8679
manguito088 <andreaequispe@gmail.com>
2024-08-27 01:15:55 -0500

    diseño_login

--- app/includes/header.php
+++ app/includes/header.php
@@ -6,17 +6,29 @@ require_once('authorize_session.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BANDA</title>
 
-    <!--complemento Font Awesome-->
+    <!-- Complemento Font Awesome -->
     <link rel="stylesheet" href="<?php echo base_url ?>plugins/fontawesome-free/css/all.min.css">
-    <!--complemento adminlte-->
+
+    <!-- Complemento AdminLTE -->
     <link rel="stylesheet" href="<?php echo base_url ?>assets/css/adminlte.css">
 
-    <!--complemento JQuery-->
+    <!-- Complemento Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Complemento Tailwind CSS -->
+    <!--<script src="https://cdn.tailwindcss.com"></script>-->
+
+    <!-- Complemento JQuery -->
     <script src="<?php echo base_url ?>plugins/jquery/jquery.min.js"></script>
-    <!--complemento Tailwind-->
-    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Script de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Script para definir base URL globalmente -->
     <script>
         var _base_url_ = '<?php echo base_url ?>';
     </script>
+
+    <!-- Script de Login personalizado -->
     <script src="<?php echo base_url ?>assets/js/login.js"></script>
 </head>
--- app/views/login.php
+++ app/views/login.php
@@ -4,43 +4,68 @@
 <!DOCTYPE html>
 <html lang = "en" class = "" style = "height: auto;">
   <?php require_once(base_app . '../app/includes/header.php') ?>
+  <head>
+    <link rel="stylesheet" href="<?php echo base_url ?>assets/css/login.css">
+  </head>
     <body>
       <script>
         start_loader()
       </script>
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-              <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-              <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Inicia sesión en tu cuenta</h2>
+
+        <!----------------------- Main Container -------------------------->
+
+        <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+        <!----------------------- Login Container -------------------------->
+
+        <div class="row border rounded-5 p-3 bg-white shadow box-area" style="width: 90%; max-width: 1200px;"> <!-- Ajusta el ancho con width y max-width -->
+
+        <!--------------------------- Left Box ----------------------------->
+
+            <div class="col-md-5 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
+                <div class="featured-image mb-3">
+                    <img src="../../assets/images/imglogin.jpg" class="img-fluid" style="width: 250px;">
+                </div>
+                <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Ingresa</p>
+                <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Accede a tu plataforma de gestión.</small>
+            </div> 
+
+        <!-------------------- ------ Right Box ---------------------------->
+
+            <div class="col-md-7 right-box"> <!-- Aumentar el tamaño de la columna derecha -->
+                <div class="row align-items-center">
+                    <form id="login-frm" class="space-y-6" action="" method="POST">
+                        <div class="header-text mb-4">
+                            <h3>¡Hola!</h3>
+                            <p>Estamos felices de tenerte de vuelta.</p>
+                        </div>
+                        <div>
+                            <label for="code" class="block text-sm font-medium leading-6 text-gray-900">Código de Socio</label>
+                            <div class="input-group mb-3">
+                                <input name="username" type="text" class="form-control form-control-lg bg-light fs-6" autofocus placeholder="N° de socio">
+                            </div>
+                        </div>
+                        <div>
+                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+                            <div class="input-group mb-1">
+                                <input name="password" type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Contraseña">
+                            </div>
+                        </div>
+
+                        <div class="input-group mb-5 d-flex justify-content-between">
+                            <div class="forgot">
+                                <small><a href="#">¿Olvidaste tu contraseña?</a></small>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button type="submit" class="btn btn-lg btn-primary w-100 fs-6">Ingresar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-          
-            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form id="login-frm" class="space-y-6" action="" method="POST">
-                <div>
-                  <label for="code" class="block text-sm font-medium leading-6 text-gray-900">Código de Socio</label>
-                  <div class="mt-2">
-                    <input name="username" type="text" class="form-control" autofocus placeholder="Usuario" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  </div>
-                </div>
-          
-                <div>
-                  <div class="flex items-center justify-between">
-                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
-                    <div class="text-sm">
-                      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">¿Olvidaste tu contraseña?</a>
-                    </div>
-                  </div>
-                  <div class="mt-2">
-                    <input name="password" type="password" class="form-control" placeholder="Contraseña" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  </div>
-                </div>
-          
-                <div>
-                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Iniciar Sesión</button>
-                </div>
-              </form>
-            </div>
-          </div>
+
+        </div>
+        </div>
 
           <script>
             $(document).ready(function(){
@@ -49,5 +74,5 @@
           </script>
 
     </body>
+    
 </html>
-
